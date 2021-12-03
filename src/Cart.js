b38486a7f03acd4633f1aea46801ef7d01d85339
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-12-03 23:40:02 +0530

    Handling inc and dec of quantity

--- src/Cart.js
+++ src/Cart.js
@@ -31,12 +31,44 @@ class Cart extends React.Component {
         }
     }
 
+    handleIncreaseQuantity = (product) => {
+        const {products} = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+
+        this.setState({
+            //products: products
+            products
+        })
+    }
+
+    handleDecreaseQuantity = (product) => {
+        const {products} = this.state;
+        const index = products.indexOf(product);
+
+        if (products[index].qty === 0) {
+            return;
+        }
+
+        products[index].qty -= 1;
+
+        this.setState({
+            products
+        })
+    }
+
     render() {
         const {products} = this.state;
         return (
             <div className='cart'>
                 {products.map((product) => {
-                    return <CartItem product={product} key={product.id}/>
+                    return <CartItem
+                    product={product}
+                    key={product.id}
+                    onIncreaseQuantity={this.handleIncreaseQuantity}
+                    onDecreaseQuantity={this.handleDecreaseQuantity}
+                    />
                 })}
             </div>
         );
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,28 +1,6 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    increaseQuantity = () => {
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty + 1
-            }
-        });
-    }
-
-    decreaseQuantity = () => {
-        const {qty} = this.state;
-
-        if (qty === 0) {
-            return;
-        }
-
-        this.setState((prevState) => {
-            return {
-                qty: prevState.qty - 1
-            }
-        });
-    }
-
     render() {
         const { price, title, qty } = this.props.product;
         return (
@@ -40,13 +18,13 @@ class CartItem extends React.Component {
                             alt="increase"
                             className="action-icons"
                             src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1638538800~hmac=b0b1b4cbe6d0eb0ab7757a033eeec0d7"
-                            onClick={this.increaseQuantity}
+                            onClick={() => this.props.onIncreaseQuantity(this.props.product)}
                         />
                         <img
                             alt="decrease"
                             className="action-icons"
                             src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
-                            onClick={this.decreaseQuantity}
+                            onClick={() => this.props.onDecreaseQuantity(this.props.product)}
                         />
                         <img
                             alt="delete"
