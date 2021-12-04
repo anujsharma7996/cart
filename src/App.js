1912e443fd99f2f6611f9b3c03b5879205d08272
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-12-04 23:53:25 +0530

    Added Cart Item count

--- src/App.js
+++ src/App.js
@@ -1,14 +1,97 @@
-import React from 'react';
-import Cart from './Cart';
-import NarBar from './NavBar';
+import React from "react";
+import Cart from "./Cart";
+import NarBar from "./NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <NarBar />
-      <Cart />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 15000,
+          title: "Mobile Phone",
+          qty: 2,
+          img: "",
+          id: 1,
+        },
+        {
+          price: 50000,
+          title: "Laptop",
+          qty: 1,
+          img: "",
+          id: 2,
+        },
+        {
+          price: 8000,
+          title: "Smart Watch",
+          qty: 3,
+          img: "",
+          id: 3,
+        },
+      ],
+    };
+  }
+
+  handleIncreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      //products: products
+      products,
+    });
+  };
+
+  handleDecreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if (products[index].qty === 0) {
+      return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products,
+    });
+  };
+
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+    const items = products.filter((item) => item.id !== id);
+
+    this.setState({
+      products: items,
+    });
+  };
+
+  getCartCount = () => {
+    const { products } = this.state;
+    let count = 0;
+
+    products.forEach((product) => {
+      count += product.qty;
+    });
+
+    return count;
+  };
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <NarBar count={this.getCartCount()} />
+        <Cart
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,39 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const NarBar = (props) => {
-        return (
-            <div style={styles.nav}>
-                <div style={styles.cartIconContainer}>
-                    <img style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/128/34/34568.png" alt="cart-icon"/>
-                    <span style={styles.cartCount}>3</span>
-                </div>
-            </div>
-        );
-    }
+  return (
+    <div style={styles.nav}>
+      <div style={styles.cartIconContainer}>
+        <img
+          style={styles.cartIcon}
+          src="https://cdn-icons-png.flaticon.com/128/34/34568.png"
+          alt="cart-icon"
+        />
+        <span style={styles.cartCount}>{props.count}</span>
+      </div>
+    </div>
+  );
+};
 
 const styles = {
   cartIcon: {
     height: 32,
-    marginRight: 20
+    marginRight: 20,
   },
   nav: {
     height: 70,
-    background: '#4267b2',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+    background: "#4267b2",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   cartIconContainer: {
-    position: 'relative'
+    position: "relative",
   },
   cartCount: {
-    background: 'yellow',
-    borderRadius: '50%',
-    padding: '4px 8px',
-    position: 'absolute',
+    background: "yellow",
+    borderRadius: "50%",
+    padding: "4px 8px",
+    position: "absolute",
     right: 0,
-    top: -9
-  }
+    top: -9,
+  },
 };
 
 export default NarBar;
